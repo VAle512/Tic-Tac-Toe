43a7717e111dc90e970d13714b76a976aed1b494
Pho3b <andrea.rinaldi54@gmail.com>
2019-01-03 15:42:53 +0100

    reset function

--- game_logic.js
+++ game_logic.js
@@ -1,4 +1,5 @@
-//Proprietà
+
+//Proprieta
 var x_turn = true;
 var victoryPositions = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
 
@@ -129,18 +130,13 @@ function checkForVictory(){
 					alert("Ha vinto " + current_winner.toUpperCase()); 
 					resetGameBoard();
 					manageScore(current_winner);
-				}, 300);
+				}, 100);
 
 				break;
 			}
 		}
 	}
 }
-
-
-
-
-
 
 
 
